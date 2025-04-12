a8a28febf17f180b6e98d493a41d58e323e5be89
Joseph Huckaby <jhuckaby@gmail.com>
2025-04-12 16:37:40 -0700

    Version 2.2.1
    - Bump `form-data` to v4.0.2 for various fixes and warnings in Node.js v22.
    - Bump `errno` to v1.0.0 because it was very old.
    - Fix keep-alive behavior in unit tests for Node.js v22.

--- test/test.js
+++ test/test.js
@@ -14,7 +14,8 @@ var PixlRequest = require('..');
 var request = new PixlRequest();
 
 var http = require('http');
-var agent = new http.Agent({ keepAlive: true });
+var agent_ka = new http.Agent({ keepAlive: true });
+var agent_na = new http.Agent({ keepAlive: false });
 
 process.chdir( __dirname );
 
@@ -1470,7 +1471,7 @@ module.exports = {
 		function testKeepAlives(test) {
 			// test keep-alive sockets
 			var sendReqGetSocketID = function(ka, callback) {
-				request.json( 'http://127.0.0.1:3020/json', false, { agent: ka ? agent : null },
+				request.json( 'http://127.0.0.1:3020/json', false, { agent: ka ? agent_ka : agent_na },
 					function(err, resp, json, perf) {
 						if (err && !json && !json.socket_id) return callback(false);
 						callback( json.socket_id );
